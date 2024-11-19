cd16089d487ceb2088c0d0049395eff644ee23d6
nondairyneutrino <chapmann19@gmail.com>
2024-11-19 14:42:35 -0800

    fix go hello

--- go/hello.go
+++ go/hello.go
@@ -1,6 +1,14 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main(){
-	fmt.Println("Hello World!")
+	if len(os.Args) > 1 {
+		name := os.Args[1] // assign the first command line argument to name
+		fmt.Println("Hello", name, "!")
+	} else {
+
+	}
 }
